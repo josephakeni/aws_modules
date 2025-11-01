3a99832a8f8daf5ad47b62ca784eb710727ccc05
joseph akeni <josephakeni@gmail.com>
2025-11-01 00:04:00 +0000

    Updated proxmox vm creation

--- proxmox/var.tf
+++ proxmox/var.tf
@@ -1,6 +1,3 @@
-variable "ssh_key" {
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDC57bYGFpcQ4DQFLDK5bIFPiU5trl7vVpAux9aExkqN0ck1D7ZJlNzmmtGEOiw59sxOXuw/3XPP9riWNyESxQgsazjSBx4LyVhXGzIYqpBvLl5wI7vU6WqVNR+zlNAVYqQGqc6nGqBv9pP7bBVuez9ZyzCAA9CNBo8rdkns88+zH+z/OwHlTkop2Z8t71A6WFk53304NVYF0HnejXL2fMV8ncGpdvcs4I7u5fJZ4lfTpi0NYhz9JBozrulwcijGk1+R7I68XW/rwL0Ac2+bBxL6F9cc5+hXWJsMMCmEwZzo0hGqNVkoqAIs3mLLK4Tuq7bp/FxKlKhSnOvh2wx/T3XT2QoK6RUSs95XTINCFDAdbonmyQVI0Dlv7Nx9a/AYKHQZggcWLVRecrPS5Z1gUiP21UtzgOJ14UTUi6vRjv/szadNw/FNWIxepniTYZut9KZaApRLEsjgSw6KRpmXucpGs7jxf1KVi6NAEDgACLCOv9NmDtPKl+PAWzH5eLt8x8= josephakeni@gmail.com"
-}
 
 variable "proxmox_host" {
   default = "pve"
@@ -10,14 +7,9 @@ variable "template_name" {
   default = "packer-test"
 }
 
-variable "proxmox_api_url" {
-  default = "https://192.168.0.3:8006/api2/json"
-}
+variable "proxmox_api_url" {}
 
 variable "proxmox_api_token" {
-  type        = string
-  default     = "terraform@pam!terraform=78cf6eac-e31d-4b83-ae1a-69bdf2956484"
-  description = "description"
 }
 
 
